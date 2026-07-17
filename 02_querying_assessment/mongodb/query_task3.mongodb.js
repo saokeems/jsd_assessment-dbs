9eb862930fe457b351110988dfe54af9e5c78b00
saokeems <saokeems@gmail.com>
2026-07-17 11:38:52 +0700

    done query_task3

--- 02_querying_assessment/mongodb/query_task3.mongodb.js
+++ 02_querying_assessment/mongodb/query_task3.mongodb.js
@@ -5,6 +5,12 @@
 //
 // Hint: Write a query to find all ingredients in the ingredients collection that have a stock_level of 100.00 or more.
 
+use("chrome-burger-db");
+
+db.ingredients.find({
+  stock_level: { $gte: 100.0 },
+});
+
 // Bonus: The dataset is identical in the PostgreSQL database, meaning the same business insight can be retrieved.
 // Write the equivalent query for PostgreSQL. See query_task3_bonus.sql
 
@@ -17,4 +23,6 @@
 // Write in English or Thai. Do not skip this step.
 //
 // Your thinking:
-//
+// โจทย์ต้องการให้เราหา ingredients ที่ stock มากกว่าเท่ากับ 100
+// ผมก็จะเข้าถึง ingredients ก่อน แล้วหลังจากนั้นก็จะกำหนดใน query ให้หาเฉพาะ stock_level ที่มากกว่าเท่ากับ 100
+// โดยใช้คำสั่ง $gte มาช่วยกรอง
